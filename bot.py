bffb1625ee38380b1e38536f12a8eb8f6e2ac503
André de Fazzio Posman <andre.posman@gmail.com>
2021-10-21 13:40:50 -0300

    improvements

--- bot.py
+++ bot.py
@@ -15,19 +15,31 @@ bot = telebot.TeleBot(os.environ["API_KEY"])
 @bot.message_handler(commands=['start'])
 def reply(message):
   print(message)
-  bot.reply_to(message, 'Vai Palmeiras')
+  if message.from_user.username == 'andreposman':
+    bot.reply_to(message, 'Oi Mestre, Avanti Palestra!')
 
+  elif message.username == 'raafvargas':
+    bot.reply_to(message, f'Eai {message.first_name}, Rei do PHP!')
+
+  elif message.username == 'Matheus':
+    bot.reply_to(message, 'Eai {message.first_name}, Rei do Spring!')
+
+  else:
+    bot.reply_to(message, 'Olá {message.first_name}, Vai Palmeiras!')
+  
 
 def extract_arg(arg):
   print(arg.split()[1:])
   return arg.split()[1:]
 
-def handleEtf(message, bot, data):
+def handleInput(data, message, stock):
+  if data.info['regularMarketPrice'] == None:
+    return True
+
+def handleETF(message, bot, data):
   if data.info['quoteType'] == 'ETF':
     if data.info['regularMarketPrice'] != None:
-      print(json.dumps(data.info))
-      # price = data.info['regularMarketPrice']
-      replyMsg = f"{data.info['symbol']} current price is: ${data.info['regularMarketPrice']}"
+      replyMsg = f"Hey {message.from_user.first_name}, {data.info['symbol']} current price is: ${data.info['regularMarketPrice']}"
 
       bot.reply_to(message, replyMsg)
 
@@ -36,25 +48,50 @@ def handleEquity(message, bot, data):
       if data.info['regularMarketPrice'] != None:
         print(json.dumps(data.info))
         price = data.info['currentPrice']
-        replyMsg = f"{data.info['symbol']} current price is: ${price}"
+        replyMsg = f"Hey {message.from_user.first_name}, {data.info['symbol']} current price is: ${price}. 💵"
 
         bot.reply_to(message, replyMsg)
 
-@bot.message_handler(commands=['stock'])
-def get_stocks(message):
-  stocks = extract_arg(message.text.upper())
-  if len(stocks) <=0:
-    bot.reply_to(message, f"You have to type a stock ticker")
+"""
+def foo():
+    while True:
+        try:
+            foo2()
+        except:
+            pass
+        else:
+            break
+"""
 
-  for stock in stocks:
-    data = yf.Ticker(stock)
+def runBot(bot):
+  @bot.message_handler(commands=['stock'])
+  def get_stocks(message):
+    stocks = extract_arg(message.text)
+    if len(stocks) <=0:
+      bot.reply_to(message, f"Yo {message.from_user.first_name}, you have to send me stock ticker. 🙄")
 
-    # if data.info['regularMarketPrice'] != None and data.info['regularMarketPrice'] != None:
-    #   bot.reply_to(message, f"No data available for: {stock}")
-    #   break
+    for stock in stocks:
+      data = yf.Ticker(stock)
+      print(data.info)
 
-    handleEtf(message, bot, data)
-    handleEquity(message, bot, data)
+      if handleInput(data, message, stock):
+        bot.reply_to(message, f"Yo {message.from_user.first_name}, I found no data available for: {stock}. 🤔")
+
+      else:
+        handleETF(message, bot, data)
+        handleEquity(message, bot, data)
 
 
-bot.polling()
+  bot.polling()
+
+def main():
+    while True:
+        try:
+            runBot(bot)
+        except:
+            pass
+        else:
+            break
+
+if __name__ == "__main__":
+  main()
