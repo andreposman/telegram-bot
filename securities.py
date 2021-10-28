5b3de9533ba3f8b0c8ec913383f9e783ef5f39f9
André de Fazzio Posman <andre.posman@gmail.com>
2021-10-28 00:33:42 -0300

    removing print()

--- securities.py
+++ securities.py
@@ -3,10 +3,6 @@ import utils.messages
 
 
 def handle_assets(message, bot, data):
-    print("-----------------------------------------")
-    print(data.info['quoteType'])
-    print("-----------------------------------------")
-
     try:
       replyMsg = utils.messages.others(message, data)
       bot.reply_to(message, replyMsg, parse_mode='Markdown')
